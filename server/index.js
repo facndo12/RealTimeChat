8fc1840c70ba85d077db87516f67f3143635c6a3
Facundo Agustin Moreira <facundomoreira12@hotmail.com>
2025-10-21 14:17:55 -0300

    Integracion de NEON como base de datos ademas de creacion de archivo .ENV para manejar variables de entorno

--- server/index.js
+++ server/index.js
@@ -1,16 +1,38 @@
 import express from 'express'
 import logger from 'morgan'
+import dotenv from 'dotenv'
+
+import pkg from 'pg'
+const { Client } = pkg;
 
 import { Server } from 'socket.io'
 import { createServer } from 'node:http'
+import { url } from 'node:inspector';
 
+dotenv.config()
 const port = process.env.PORT ?? 3000
 
+const db = new Client({
+    connectionString: "postgresql://neondb_owner:npg_OU08uRhEfBtd@ep-small-water-ae0ytnxt-pooler.c-2.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+    ssl: { rejectUnauthorized: false } // necesario para Neon
+});
+
+await db.connect()
 const app = express()
 const server = createServer(app)
 const io = new Server(server, {
     connectionStateRecovery:{}
 })
+
+
+await db.query(`
+    CREATE TABLE IF NOT EXISTS messages (
+    id SERIAL PRIMARY KEY,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)
+    `
+)
+
 
 io.on('connection', (socket) => {
     console.log('user has connected')
@@ -19,8 +41,19 @@ io.on('connection', (socket) => {
         console.log('Usser has disconnected')
     })
 
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg)
+    socket.on('chat message', async(msg) => {
+        let result 
+        try {
+            result = await db.query(
+                `INSERT INTO messages (content) VALUES ($1) RETURNING *`,
+                [msg]
+            )
+            
+            const insertedMessage = result.rows[0]
+            io.emit('chat message', msg, insertedMessage.id.toString())
+        } catch (e){
+            console.error('Error saving message:', e)
+        }
     })
 })
 
